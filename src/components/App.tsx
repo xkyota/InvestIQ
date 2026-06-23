e800565c3cb667a38cab4380cb95f26219769c35
Maksym Durikhin <max1mkasss.mail@gmail.com>
2026-06-23 22:09:37 +0300

    Section Header
    Setted up section header, section's header styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,13 @@
 import '../styles/App.css';
 
+import Header from '../components/Header';
+
 function App() {
-  return;
+	return (
+		<header className='section-header'>
+			<Header></Header>
+		</header>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import '../styles/Header.css';
+
+//! Images
+import defaultUser from '../assets/default-user.svg';
+import logo from '../assets/logo.svg';
+import separator from '../assets/separator.svg';
+
+function Header() {
+	return (
+		<>
+			<div className="logo-wrapper">
+				<img src={logo} alt="logo" />
+				<span>InvestIQ</span>
+			</div>
+
+			<div className="account-management">
+                <img src={defaultUser} alt="Default User Icon" />
+                <span id='userName'>User Name</span>
+
+                <img className='separator' src={separator} alt="" />
+                <span id='exitBtn'>Вийти</span>
+            </div>
+		</>
+	);
+}
+
+export default Header;
